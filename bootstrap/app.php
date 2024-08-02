ef090997ffb0c856f79320167579d018e78349a4
vindrayulian <vindrayulian@gmail.com>
2024-08-02 11:25:23 +0700

    fix disableCache

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\AdminAccess;
+use App\Http\Middleware\DisableCache;
 use Illuminate\Foundation\Application;
 use App\Http\Middleware\SuperAdminAccess;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -17,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'adminAccess' => AdminAccess::class,
             'superAdminAccess' => SuperAdminAccess::class,
+            'disableCache' => DisableCache::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
